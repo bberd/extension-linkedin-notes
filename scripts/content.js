cdb19d7b23fff96c0946788ccd52e38cbcf79d37
Boris Berdichevskiy <bberdich@gmail.com>
2018-03-22 15:39:26 -0400

    feat: prepend NOTES SECTION to HTML

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,11 @@
-const insertSection = document.getElementsByClassName("pv-content__right-rail right-rail");
+let insertSection = document.getElementsByClassName("pv-content__right-rail right-rail");
+insertSection = Array.prototype.slice.call(insertSection)[0];
+var insertSection2 = "h2[contains(text(),'Contact and Personal Info')]"
 const notes = document.createElement("div");
 notes.innerHTML += 'NOTES SECTION';
-
-insertSection.innerHTML += notes;
+console.log(insertSection);
+//console.log(insertSection2);
+//insertSection.innerHTML += notes.innerHTML;
+console.log(insertSection);
 // insertSection.insertBefore(notes, insertSection.firstChild);
+insertSection.prepend(notes);
