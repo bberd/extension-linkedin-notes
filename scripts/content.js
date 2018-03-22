b27a33117fa3dd10271793d4cd8e764167e82846
Boris Berdichevskiy <bberdich@gmail.com>
2018-03-22 15:53:17 -0400

    added textarea

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,13 @@
-let insertSection = document.getElementsByClassName("pv-content__right-rail right-rail");
-insertSection = Array.prototype.slice.call(insertSection)[0];
-var insertSection2 = "h2[contains(text(),'Contact and Personal Info')]"
+let whereToInsert = document.getElementsByClassName("pv-content__right-rail right-rail");
+[whereToInsert] = whereToInsert;
 const notes = document.createElement("div");
-notes.innerHTML += 'NOTES SECTION';
-console.log(insertSection);
-//console.log(insertSection2);
-//insertSection.innerHTML += notes.innerHTML;
-console.log(insertSection);
-// insertSection.insertBefore(notes, insertSection.firstChild);
-insertSection.prepend(notes);
+notes.innerHTML = `
+  Notes
+  <div>
+    <textarea name="note">
+    </textarea>
+  </div>
+`;
+console.log(whereToInsert, notes);
+whereToInsert.insertBefore(notes, whereToInsert.firstChild);
+//whereToInsert.prepend(notes);
