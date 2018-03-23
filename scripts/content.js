64ae3c8dc10b9779d90a6fb6418e73a4fa36ccb3
Boris Berdichevskiy <bberdich@gmail.com>
2018-03-23 18:37:05 -0400

    added persistance

--- scripts/content.js
+++ scripts/content.js
@@ -1,20 +1,29 @@
-let whereToInsert = document.getElementsByClassName("pv-content__right-rail right-rail");
-[whereToInsert] = whereToInsert;
-const notes = document.createElement("div");
-notes.innerHTML = `
-  Notes
+const contactId = window.location.pathname.split('/')[2];
+const [whereToInsert] = document.getElementsByClassName("pv-content__right-rail right-rail");
+const notesDiv = document.createElement("div");
+let prevNoteText = '';
+
+notesDiv.innerHTML =
+`Notes
   <div>
-    <textarea id='textAreaNote' name="note" placeholder="Enter any notes here">
-    </textarea>
+    <textarea id='noteTextArea' name="note" placeholder="Enter any notes here"></textarea>
   </div>
-  <button id="noteSaveBtn">Save</button>
-`;
-console.log(whereToInsert, notes);
-whereToInsert.insertBefore(notes, whereToInsert.firstChild);
+  <button id="noteSaveBtn">
+    Save
+  </button>`;
+
+whereToInsert.insertBefore(notesDiv, whereToInsert.firstChild);
+
+chrome.storage.sync.get([contactId], function (idsInStorage) {
+  prevNoteText = idsInStorage[contactId];
+  document.getElementById("noteTextArea").value = prevNoteText || '';
+});
 
 document.getElementById("noteSaveBtn").addEventListener("click", saveNote)
 
 function saveNote() {
-  var notesText = document.getElementById('textAreaNote').value;
-  console.log(notesText);
+  const contactObj = {}
+  newNoteVal = document.getElementById('noteTextArea').value;
+  contactObj[contactId] = newNoteVal;
+  chrome.storage.sync.set(contactObj);
 }
